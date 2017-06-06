a543e476d6967cee768273ecb4adc039a6de0e64
Purgatorius <Bartlomiej.Bugala@gmail.com>
2017-06-06 18:04:33 +0200

    Create code_CircleCircumferenceArea.java

---
+++ code_CircleCircumferenceArea.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * Created by B4RT on 2017-05-10.
+ */
+public class CircleCircumferenceArea {
+    public static void main (String args[])
+    {
+        System.out.println("This program calculates circumference and area of given radius (r) of circle");
+        int r;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("r=");
+        r = scanner.nextInt();
+        System.out.println("Pole = " + (r*r*3.14));
+        System.out.println("Obwód = " + (2*3.14*r));
+
+
+    }
+}
